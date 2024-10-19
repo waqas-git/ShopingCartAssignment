e1e84f28a73ade39333417c706940e97847f0274
Waqas Ahmed <waqasahmed.uol@gmail.com>
2024-10-20 00:57:31 +0500

    Initial Commit

---
+++ ShopingCartAssignment/ShopingCartAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopingCartAssignmentApp.swift
+//  ShopingCartAssignment
+//
+//  Created by waqas ahmed on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShopingCartAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
